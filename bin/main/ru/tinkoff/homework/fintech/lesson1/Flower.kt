f4c28acaf65da3e83d14935c419ced2ab4457449
Anna Tselikova <ania.tselikova@gmail.com>
2022-04-22 23:14:09 +0300

    fix: wip

---
+++ bin/main/ru/tinkoff/homework/fintech/lesson1/Flower.kt
@@ -0,0 +1,31 @@
+package ru.tinkoff.homework.fintech.lesson1
+
+
+class Flower(override val name: String, private var isBisexual: Boolean = true) : Plant {
+    /*
+    * This example demonstrates a possibility to add an additional method
+    * beside the interface's methods.
+    * In this example: an additional property and an additional method
+    */
+    fun breed() {
+        if (isBisexual) {
+            println("$name has sprouted! Look out for new leaves!")
+        } else {
+            println("You need $name of other sex in order to breed")
+        }
+    }
+
+    /*
+    * This example demonstrates an implementation of the overriding method in the interface
+    */
+    override fun blossom() {
+        println("$name is blossoming! The flower smells pleasantly fragrant")
+    }
+
+    /*
+     * This example demonstrates an implementation of an abstract method in the interface
+     */
+    override fun dry() {
+        println("$name is drying out. Use the watering can to water it")
+    }
+}
---
+++ bin/main/ru/tinkoff/homework/fintech/lesson3/Tree.kt
@@ -0,0 +1,31 @@
+package ru.tinkoff.homework.fintech.lesson3
+
+import kotlin.math.max
+import kotlin.math.min
+
+class Tree(override val name: String, private val height: Int, private var hydration: Int = 100) : Plant {
+    init {
+        /*
+         * Bring `hydration` parameter to range 0-100.
+         * TODO throw an exception instead.
+         */
+        hydration = max(hydration, 0)
+        hydration = min(hydration, 100)
+    }
+    /*
+    * This example demonstrates a possibility to add an additional method
+    * beside the interface's methods.
+    * In this example: an additional property and an additional method
+    */
+    fun getHeightInfo() {
+        println("The tree is $height cm. Please bear it in mind when planning your planting location")
+    }
+
+    override fun dry() {
+        println("$name is running out of water. Save the tree before it is too late!")
+    }
+
+    override fun getHydrationLevel(): Int {
+        return hydration
+    }
+}
